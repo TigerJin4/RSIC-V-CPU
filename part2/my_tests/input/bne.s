c0181597163db4fbe18d56ba5dc8c50e4c100678
Wenhan Jin <tigerjin4@berkeley.edu>
2019-03-22 17:23:09 -0700

    ll

--- part2/my_tests/input/bne.s
+++ part2/my_tests/input/bne.s
@@ -1,22 +1,11 @@
-	bne s0 s0 start #0
-bad-loop: 
-	addi sp sp -1 #1
-	beq x0 x0 bad-loop #2
+addi s1 s1 -20
+beq s0 s0 label2
 
-start:
-	addi s1 s1 10#3
-	blt s0 s1 label1#4
-	beq x0 x0 bad-loop#5
-
-label2: 
-	addi s1 s1 -20#6
-	bltu s0 s1 end#7
-    beq x0 x0 bad-loop#8
+label2:
+   	bne s1 x0 end#8
 
 label1: 
 	addi s0 s0 20#9
-	blt s1 s0 label2#10
-    bne x0 x0 bad-loop#11
 
 end:
-	add a0 x0 x0#12
+	addi a0 x0 6#12
---
+++ part2/my_tests/input/slti.s
@@ -0,0 +1,2 @@
+addi t0, x0, 5
+slti t1, t0, 3
