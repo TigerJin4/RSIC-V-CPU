579c6659f7aaa26ef5bbde68c17b3d34926b36aa
Wenhan Jin <tigerjin4@berkeley.edu>
2019-03-22 19:46:29 -0700

    ll

--- part2/mulh.s
+++ part2/mulh.s
@@ -2,3 +2,9 @@ addi t0, x0, 5
 addi t1, x0, 3
 mulh t2, t0, t1
 addi s0, t2, 9
+
+addi t0, x0, 6
+addi t1, x0, 4
+mulh s0, t0, t1
+addi t0, x0, -6
+mulh s1, t0, t1
--- part2/my_tests/input/divu.s
+++ part2/my_tests/input/divu.s
@@ -1,3 +1,9 @@
 addi t0, x0, 14
 addi t1, x0, 3
 divu t2, t0, t1
+
+addi t0, x0, 6
+addi t1, x0, 3
+divu s0, t0, t1
+addi t2, x0, -6
+divu s1, t2, t1
