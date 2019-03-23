3e84fce8904b52025b2dfa85f25b152d210f781c
Wenhan Jin <tigerjin4@berkeley.edu>
2019-03-22 19:16:44 -0700

    ll

--- part2/xor.s
+++ part2/xor.s
@@ -2,3 +2,9 @@ addi t0, x0, 5
 addi t1, x0, 3
 xor t2, t0, t1
 xor s1, t2, x0
+
+addi t0, x0, 6
+addi t1, x0, 4
+addi t2, x0, -6
+xor s0, t0, t1
+xor s1, t2, t1
--- part2/xori.s
+++ part2/xori.s
@@ -1,2 +1,7 @@
 addi t0, x0, 5
 xori t1, t0, 3
+
+addi t0, x0, 6
+addi t2, x0, -6
+xori t1, t0, 2
+xori s0, t2, -2
